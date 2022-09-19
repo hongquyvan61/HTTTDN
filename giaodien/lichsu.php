f3d2379e0c6584af793dd648c4ac2075506b018a
hongquyvan61 <95014131+hongquyvan61@users.noreply.github.com>
2022-09-19 20:11:19 +0700

    fix lỗi tạo đơn hàng + thêm tính năng lọc ở lịch sử giao hàng

--- giaodien/lichsu.php
+++ giaodien/lichsu.php
@@ -43,6 +43,45 @@
            </footer>
         </div>
     </body>
-   
+   <script type="text/javascript">
+            
+            $(document).ready(function(){
+                function statusmacdinh(){
+                    var macdinh = document.getElementById('slboxsort').value;
+                    guiajax(macdinh);
+                }
+                function guiajax(status){
+                    var user = <?php echo $_SESSION['id'];?>;
+                    $.ajax({
+                            method: 'post',
+                            url: '../model/ajax_loctinhtrang.php',
+                            datatype: "JSON",
+                            data: {userid: user,tinhtrang: status},
+                            success: function(response){
+                                var arrayObj = JSON.parse(response);
+                                $("#tablelichsu").find('tbody').empty();
+                                arrayObj.forEach(function (item,index){
+                                    var row = $('<tr>');
+                                    row.append('<td>' + item.billid+ '</td>');
+                                    row.append('<td>' + item.name + '</td>');
+                                    row.append('<td>' + item.size + '</td>');
+                                    row.append('<td>' + item.soluong + '</td>');
+                                    row.append('<td>' + item.ngaythanhtoan + '</td>');
+                                    row.append('<td style="color:#92f200; font-weight: bold;">' + item.tinhtrang + '</td>');
+                                    row.append('</tr>');
+                                    $("#tablelichsu").find('tbody').append(row);
+                                });
+                            }
+                    });
+                }
+                function checkselect(){
+                    var statusdachon = document.getElementById('slboxsort').value;
+                    guiajax(statusdachon);
+                }
+                statusmacdinh();
+                document.getElementById("slboxsort").onchange = checkselect;
+            });
+            
+   </script>
 </html>
 
--- xuly/xulylichsu.php
+++ xuly/xulylichsu.php
@@ -1,12 +1,46 @@
-<table class="table table-bordered table-striped">
+<?php 
+    require '../connectdb/connect.php';
+    $con = ketnoi();
+    $user_id=$_SESSION['id'];
+    $statusquery = "select tinh_trang from don_hang where user_id=$user_id and tinh_trang='Paid' or tinh_trang='Shipped' GROUP BY tinh_trang";
+    $statusres = mysqli_query($con,$statusquery);
+    ?>
+    <select id="slboxsort">
+        <option selected="true">All</option>
+<?php
+    if(mysqli_num_rows($statusres) != 0){
+        
+?>
+        
+            <?php while($statusrow = mysqli_fetch_assoc($statusres))
+            {
+                ?>
+                <option><?php echo $statusrow['tinh_trang'];?></option>
+            <?php 
+            }
+       ?>
+<?php 
+     }
+?>
+    </select>
+<table id="tablelichsu" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                    <th>Bill_id</th>
+                                    <th>Name</th>
+                                    <th>Size</th>
+                                    <th>Quantity</th>
+                                    <th>Date of payment</th>
+                                    <th>Status</th>
+
+                           </tr>
+                        </thead>
                         <tbody>
-                           <?php 
-                             require '../connectdb/connect.php';
-                                $con = ketnoi();
+                           <?php
                                 if(!isset($_SESSION['email'])){
                                     header('location: login.php');
                                 }
-                                $user_id=$_SESSION['id'];
+                                
                                 $user_products_query="select don.ma_don_hang,g.ten, ct.size, ct.so_luong, ct.don_gia, don.ngay_gio_thanh_toan, don.tinh_trang
                                                     from don_hang as don
                                                     join gio_hang as gio
@@ -19,28 +53,19 @@
                             $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
                             if(mysqli_num_rows($user_products_result)!=0){
                                 ?>
-                                <tr>
-                                    <th>Bill_id</th>
-                                    <th>Name</th>
-                                    <th>Size</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                    <th>Add Cart Date</th>
-                                    <th>Status</th>
-
-                                </tr>
+                                
                            <?php 
                                 while($row=mysqli_fetch_assoc($user_products_result)){
 
                              ?>
                             <tr>
-                                <th><?php echo $row['ma_don_hang']?></th>
-                                <th><?php echo $row['ten']?></th>
-                                <th><?php echo $row['size']?></th>
-                                <th><?php echo $row['so_luong']?></th>
-                                <th><?php echo $row['don_gia']?></th>
-                                <th><?php echo $row['ngay_gio_thanh_toan']?></th>
-                                <th style="color:#92f200;"><?php echo $row['tinh_trang']?></th>
+                                <td><?php echo $row['ma_don_hang']?></td>
+                                <td><?php echo $row['ten']?></td>
+                                <td><?php echo $row['size']?></td
+                                <td><?php echo $row['so_luong']?></td>
+                                <td><?php echo $row['don_gia']?></td>
+                                <td><?php echo $row['ngay_gio_thanh_toan']?></td>
+                                <td style="color:#92f200; font-weight: bold;"><?php echo $row['tinh_trang']?></td>
                             </tr>
                            <?php }
                             }
